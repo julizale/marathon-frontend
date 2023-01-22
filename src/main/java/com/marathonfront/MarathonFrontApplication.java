7a1f26ca34174fd66450ca1fb76b9c8de059edcd
Julian Zaleski <julizale@gmail.com>
2023-01-22 22:29:06 +0100

    First commit

---
+++ src/main/java/com/marathonfront/MarathonFrontApplication.java
@@ -0,0 +1,13 @@
+package com.marathonfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarathonFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarathonFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marathonfront/MarathonFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marathonfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarathonFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
